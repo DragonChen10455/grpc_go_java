e61cccda8125ed5c86e540d214f54ac72af81a65
ChenHaolong <1036749218@qq.com>
2022-08-12 11:20:30 +0800

    2022-08-12

--- grpc-client/src/test/java/com/nces/grpcclient/client/PurgeCloudflareClientTest.java
+++ grpc-client/src/test/java/com/nces/grpcclient/client/PurgeCloudflareClientTest.java
@@ -12,8 +12,8 @@ class PurgeCloudflareClientTest {
     void purgeCloudflare() {
         PurgeCloudflareClient client = new PurgeCloudflareClient();
         client.init();
-        String apiKey = "676f8785dad552acb6ea14d3e822bc41a2070";
-        String apiEmail = "haolong@centcloud.net";
+        String apiKey = "apiKey";
+        String apiEmail = "apiEmail";
         String zoneName = "rhyuheg.site";
         String[] purgeList = {"https://www.rhyuheg.site", "https://www.rhyuheg2.site/foo"};
         if(client.purgeCloudflare(apiKey, apiEmail, zoneName, purgeList)) {
